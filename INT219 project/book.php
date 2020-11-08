ae2c98c5ee1e5a6b61156a950774aca03e1f0e4a
ujjwal kumar tiwari <62056777+ujjwaltiwari07@users.noreply.github.com>
2020-11-08 16:46:24 +0530

    Add files via upload

---
+++ INT219 project/book.php
@@ -0,0 +1,162 @@
+<?php
+include('includes/tabledata.php');
+?>
+
+<html>
+<head>
+<title>RESERVATION </title>
+<link rel="stylesheet" type="text/css" href="css/table.css">
+</head>
+<body>
+
+<div class="main">
+	<div class="logo">
+	<img src="logo.png">
+	</div>
+	<ul>
+<li><a href="bb.html">Home</a></li>
+<li><a href="pallex.html">Menu</a></li>
+<li><a href="gal1.html">Gallery</a></li>
+
+<li><a href="#">Account</a>
+<ul>
+<li><a href="login.html">login</a></li>
+<li><a href="signups.html">signup</a></li></ul>
+<li><a href="about.html">About</a></li>
+</ul>
+</li>
+
+	</ul>
+</div>
+<header>
+  <div class="reser"><h1>
+<span>R</span>
+<span>E</span>
+<span>S</span>
+<span>E</span>
+<span>R</span>
+<span>V</span>
+<span>A</span>
+<span>T</span>
+<span>I</span>
+<span>O</span>
+<span>N</span>
+
+
+
+</h1></div>
+<div class="pause1">
+<p><font color="white">__________</font><font color="darkred">&hearts;&hearts;</font><font color="white">__________</font></p>
+
+
+  </div>
+
+</header>
+<div>
+<div class="section">
+	<div><p>Tel: 201122543<br>
+E-Mail: PANKAJMOZAIK.COM</p></div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3410.812548008813!2d75.70148031514414!3d31.253612153317544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a5a594d22b88d%3A0x4cc934c58d0992ec!2sLovely+Professional+University!5e0!3m2!1sen!2sin!4v1554561003816!5m2!1sen!2sin" width="600" height="430" frameborder="0" style="border:0" allowfullscreen> </iframe>
+
+
+</div>
+</div>
+<div class="form">
+<fieldset><legend>CONTACT DETAILS</legend>
+	
+		<form action="table.php" method="POST">
+  <div>
+    <label for="text">Name:</label>
+    <input type="text" name="name" >
+  </div>
+  <div >
+    <label for="text">Email:</label>
+    <input type="text" name="email"  >
+  </div>
+   <div >
+    <label for="text">Phone:</label>
+    <input type="text" name="phn"  >
+  </div>
+  
+  
+
+
+
+
+
+
+
+
+
+</fieldset>
+
+
+</div>
+<div class="cont">
+<fieldset>
+  <legend>BOOK A TABLE</legend>
+	
+
+  Date: <input type="date" name="date">
+  <div>
+  Time: <input type="time" name="time">
+  </div>
+
+  <div>
+  Member: <input type="number" name="mem">
+  </div>
+
+  <div class="submit">
+
+  <input type="submit" name="submit"  value="REQUEST BOOKING">
+
+</div>
+</form>
+
+
+
+
+
+
+
+
+</fieldset>
+
+
+</div>
+
+<div>
+
+
+
+</div>
+<div>
+<footer class="footer">
+  <div class="pause">
+<p><font color="white">__________&hearts;&hearts;__________</font></p>
+
+
+  </div>
+<div class="menu2">
+
+<ul>
+<li><a href="bb.html">Home |</a></li>
+<li><a href="pallex.html">Menu |</a></li>
+<li><a href="about.html">About |</a></li>
+<li><a href="#">Account |</a>
+<ul>
+  </ul>
+</div>
+
+</footer>
+<div class="log">
+  <img src="lo.png">
+  </div>
+
+
+
+</div>
+
+
+</body>
+</html>
